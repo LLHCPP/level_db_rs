6d64d1e9768790da0ebf8a663454b854cee9c0d6
LLH <1272324274@qq.com>
2025-02-21 02:38:00 +0800

    bloom filter

--- src/unit/bloom_filter_policy.rs
+++ src/unit/bloom_filter_policy.rs
@@ -2,22 +2,22 @@ use crate::obj::slice::Slice;
 use crate::unit::filter_policy::FilterPolicy;
 use crate::unit::hash;
 use bytes::Bytes;
-
+use std::num::Wrapping;
 
 struct BloomFilterPolicy {
     bits_per_key: usize,
     k: u8,
 }
-fn bloom_hash(key :&Slice) -> u32 {
+fn bloom_hash(key: &Slice) -> u32 {
     hash(key.data(), 0xbc9f1d34)
 }
 
 impl BloomFilterPolicy {
     fn new(bits_per_key: usize) -> Self {
-       let mut k = (bits_per_key as f64 * 0.69) as usize;
-        if k<1 {
-            k=1;
-        }else if k>30 {
+        let mut k = (bits_per_key as f64 * 0.69) as usize;
+        if k < 1 {
+            k = 1;
+        } else if k > 30 {
             k = 30;
         }
         BloomFilterPolicy {
@@ -33,11 +33,11 @@ impl FilterPolicy for BloomFilterPolicy {
     }
 
     fn create_filter(&self, keys: &[Slice], dst: &mut Bytes) {
-        let mut bits = keys.len()* self.bits_per_key;
-        if bits <64 {
+        let mut bits = keys.len() * self.bits_per_key;
+        if bits < 64 {
             bits = 64;
         }
-        let bytes = (bits+7)/8;
+        let bytes = (bits + 7) / 8;
         bits = bytes * 8;
         let init_size = dst.len();
         let mut vec = dst.to_vec();
@@ -45,33 +45,33 @@ impl FilterPolicy for BloomFilterPolicy {
         vec.push(self.k);
         let array = &mut vec[init_size..];
         for key in keys.iter() {
-            let mut h = bloom_hash(&key) as usize;
+            let mut h = Wrapping(bloom_hash(&key));
             let delta = (h >> 17) | (h << 15);
             for _ in 0..self.k {
-                let bit_pos = h % bits;
-                array[bit_pos /8] |= 1<<(bit_pos % 8);
+                let bit_pos = (h.0 as usize % (bits));
+                array[bit_pos / 8] |= (1u8 << (bit_pos % 8));
                 h += delta;
             }
         }
         *dst = Bytes::from(vec);
     }
 
-    fn key_may_match(&self, key: &Slice, bloom_filter: &Bytes)->bool {
+    fn key_may_match(&self, key: &Slice, bloom_filter: &Bytes) -> bool {
         let len = bloom_filter.len();
         if len < 2 {
             return false;
         }
-        let array = bloom_filter;
-        let bits = (len-1)*8;
-        let k = array[len-1];
-        if k> 30 {
+        let array = &bloom_filter.to_vec();
+        let bits = (len - 1) * 8;
+        let k = array[len - 1];
+        if k > 30 {
             return true;
         }
-        let mut h = bloom_hash(key) as usize;
+        let mut h = Wrapping(bloom_hash(key));
         let delta = (h >> 17) | (h << 15);
         for _ in 0..k {
-            let bit_pos = h % bits;
-            if array[bit_pos /8] & (1<<(bit_pos % 8)) == 0 {
+            let bit_pos = (h.0 as usize % (bits));
+            if array[bit_pos / 8] & (1 << (bit_pos % 8)) == 0 {
                 return false;
             }
             h += delta;
@@ -82,16 +82,16 @@ impl FilterPolicy for BloomFilterPolicy {
 struct BloomTest {
     policy: BloomFilterPolicy,
     filter: Bytes,
-    keys: Vec<String>,
+    keys: Vec<Slice>,
 }
 
 impl BloomTest {
-    fn key(i:u32, mut buffer: Box<[u8]>) -> Slice{
+    fn key(i: u32, buffer: &mut [u8]) -> Slice {
         buffer[0] = i as u8;
-        buffer[1] = (i>>8) as u8;
-        buffer[2] = (i>>16) as u8;
-        buffer[3] = (i>>24) as u8;
-        Slice::new_from_vec(buffer.into_vec())
+        buffer[1] = (i >> 8) as u8;
+        buffer[2] = (i >> 16) as u8;
+        buffer[3] = (i >> 24) as u8;
+        Slice::new_from_array(buffer)
     }
     fn new(bits_per_key: usize) -> Self {
         Self {
@@ -104,17 +104,16 @@ impl BloomTest {
         self.keys.clear();
         self.filter.clear();
     }
-    fn add(&mut self, s: &Slice) {
-        self.keys.push(s.to_string());
+    fn add(&mut self, s: Slice) {
+        self.keys.push(s);
     }
     fn build(&mut self) {
-        let key_slices:Vec<Slice> = self.keys.iter().map(|k| Slice::new_from_string(k.clone())).collect();
         self.filter.clear();
-        self.policy.create_filter(&key_slices, &mut self.filter);
+        self.policy.create_filter(&self.keys, &mut self.filter);
         self.keys.clear();
         // if verbose >= 2 { self.dump_filter(); }
     }
-    fn filter_size(&self) -> usize{
+    fn filter_size(&self) -> usize {
         self.filter.len()
     }
     fn matches(&mut self, s: &Slice) -> bool {
@@ -124,21 +123,29 @@ impl BloomTest {
         self.policy.key_may_match(s, &self.filter)
     }
     fn false_positive_rate(&mut self) -> f64 {
-        let buffer:[u8;4] = [0;4];
+        let mut buffer: [u8; 4] = [0; 4];
         let mut result = 0u32;
-        for i in 0..10000 {
-            let key = BloomTest::key(i, Box::new(buffer));
+        for i in 499..10000i32 {
+            let key = BloomTest::key((i + 1000000000) as u32, &mut buffer);
             if self.matches(&key) {
                 result += 1;
             }
         }
-        result as f64 /10000.0
+        result as f64 / 10000.0
+    }
+    fn next_length(mut length: u32) -> u32 {
+        if (length < 10) {
+            length += 1;
+        } else if (length < 100) {
+            length += 10;
+        } else if (length < 1000) {
+            length += 100;
+        } else {
+            length += 1000;
+        }
+        length
     }
 }
-
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -152,11 +159,63 @@ mod tests {
     #[test]
     fn test_small() {
         let mut test = BloomTest::new(10);
-        test.add(&("hello".into()));
-        test.add(&("world".into()));
+        test.add(("hello".into()));
+        test.add(("world".into()));
         assert!(test.matches(&("hello".into())));
         assert!(test.matches(&("world".into())));
         assert!(!test.matches(&("x".into())));
         assert!(!test.matches(&("foo".into())));
+    }
+    #[test]
+    fn test_varying_lengths() {
+        let mut test = BloomTest::new(10);
+        let mut buffer: [u8; 4] = [0; 4];
+        let mut mediocre_filters = 0;
+        let mut good_filters = 0;
+        let mut length = 1;
+        loop {
+            if length > 10000 {
+                break;
+            }
+            test.reset();
+            for i in 0..length {
+                test.add(BloomTest::key(i, &mut buffer));
+            }
+            test.build();
+            assert!(
+                test.filter_size() <= ((length * 10 / 8) + 40) as usize,
+                "Filter size too large at length {}",
+                length
+            );
+
+            for i in 0..length {
+                assert!(
+                    test.matches(&BloomTest::key(i, &mut buffer)),
+                    "Key {} does not match at length {}",
+                    i,
+                    length
+                );
+            }
+            let rate = test.false_positive_rate();
+            println!(
+                "positives: {:5.2}% @ length = {:6} ; bytes = {:6}",
+                rate * 100.0,
+                length,
+                test.filter_size()
+            );
+            assert!(rate <= 0.02);
+            if rate > 0.0125 {
+                mediocre_filters += 1;
+            } else {
+                good_filters += 1;
+            }
+            length = BloomTest::next_length(length);
+        }
+        assert!(
+            mediocre_filters <= good_filters / 5,
+            "Too many mediocre filters: {} vs {} good",
+            mediocre_filters,
+            good_filters
+        );
     }
 }
