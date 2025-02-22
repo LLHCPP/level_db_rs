0907ab0292019e93d7a121df1339314b4536dcb4
LLH <1272324274@qq.com>
2025-02-22 21:08:42 +0800

    arena

---
+++ src/obj/arena.rs
@@ -0,0 +1,43 @@
+use std::rc::Rc;
+use std::sync::atomic::AtomicU64;
+use bumpalo::Bump;
+struct Arena {
+    bump: Bump,
+}
+impl Arena {
+    fn new() -> Self {
+        Self {
+            bump: Bump::new(),
+        }
+    }
+    fn alloc<T>(&mut self, value:T) -> &mut T
+    {
+        self.bump.alloc(value)
+    }
+    // 专门为数组优化的版本
+    fn alloc_array<T>(&mut self, len: usize) -> &mut [T] {
+        let layout = std::alloc::Layout::array::<T>(len).unwrap();
+        let slice = {
+            let ptr = self.bump.alloc_layout(layout).as_ptr() as *mut T;
+            unsafe { std::slice::from_raw_parts_mut(ptr, len) }
+        };
+        slice
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::obj::arena::Arena;
+    #[test]
+    fn test_alloc() {
+        let mut test = Arena::new();
+        let buffer = test.alloc([0; 4]);
+        println!("{:?}", buffer)
+    }
+
+    #[test]
+    fn test_alloc_array() {
+        let mut test = Arena::new();
+        let buffer = test.alloc_array::<u8>(12);
+        println!("{:?}", buffer)
+    }
+}
