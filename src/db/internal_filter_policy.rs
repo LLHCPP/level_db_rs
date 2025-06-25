a919e649efdb7f411350700fa7c8ef998cada4ad
LLH <1272324274@qq.com>
2025-06-25 22:40:09 +0800

    table iter

--- src/db/internal_filter_policy.rs
+++ src/db/internal_filter_policy.rs
@@ -1,10 +1,15 @@
-use std::sync::Arc;
-use bytes::BytesMut;
 use crate::obj::slice::Slice;
 use crate::util::filter_policy::FilterPolicy;
+use bytes::BytesMut;
+use std::sync::Arc;
 
 struct InternalFilterPolicy {
-    user_policy_: Arc<dyn FilterPolicy>
+    user_policy_: Arc<dyn FilterPolicy>,
+}
+
+fn extract_user_key(internal_key: &Slice) -> Slice {
+    assert!(internal_key.len() > 8);
+    Slice::new_from_ptr(&internal_key.data()[0..internal_key.len() - 8])
 }
 
 impl FilterPolicy for InternalFilterPolicy {
@@ -13,10 +18,15 @@ impl FilterPolicy for InternalFilterPolicy {
     }
 
     fn create_filter(&self, keys: &[Slice], dst: &mut BytesMut) {
-        todo!()
+        let m_keys = keys
+            .iter()
+            .map(|key| extract_user_key(key))
+            .collect::<Vec<_>>();
+        self.user_policy_.create_filter(&m_keys, dst)
     }
 
     fn key_may_match(&self, key: &Slice, filter: &Slice) -> bool {
-        todo!()
+        self.user_policy_
+            .key_may_match(&extract_user_key(key), filter)
     }
 }
