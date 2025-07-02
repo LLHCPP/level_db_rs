be5d7d231bc7c4157db50b67053c9767fdbf3627
LLH <1272324274@qq.com>
2025-07-02 23:13:55 +0800

    memtable

--- src/db/mem_table.rs
+++ src/db/mem_table.rs
@@ -3,7 +3,7 @@ use crate::db::internal_key_comparator::{InternalKeyComparator, ValueType};
 use crate::obj::slice::Slice;
 use crate::obj::status_rs::Status;
 use crate::util::arena::Arena;
-use crate::util::coding::{encode_fixed64, encode_varint32, encode_varint64, varint_length};
+use crate::util::coding::{encode_fixed64, encode_varint32, encode_varint64, get_varint32ptr, varint_length};
 use crossbeam_skiplist::SkipMap;
 
 struct KeyComparator {
@@ -11,9 +11,16 @@ struct KeyComparator {
 }
 
 struct MemTable {
-    table: SkipMap<InternalKey, Slice>,
+    table: SkipMap<Slice, (u64, ValueType, Slice)>,
     arena: Arena,
 }
+
+fn get_length_prefixed_slice(data: &[u8]) -> Slice {
+    let mut len = 0;
+    let p =  get_varint32ptr(data, &mut len).unwrap();
+    Slice::new_from_ptr(&p[..(len as usize)])
+}
+
 
 impl MemTable {
     fn add(&self, seq: u64, value_type: ValueType, key: &Slice, value: &Slice) {
@@ -32,10 +39,26 @@ impl MemTable {
         p = &mut p[8..];
         p = encode_varint32(p, val_size as u32);
         p.copy_from_slice(value.data());
-        self.table.insert(InternalKey::new(key.clone(), seq, value_type), Slice::new_from_ptr(buf));
+        self.table.insert(key.clone(), (seq, value_type, Slice::new_from_ptr(buf)));
     }
 
-    fn Get(&self, key: &Slice) -> Result<Slice, Status> {
-        Ok(Slice::new_empty())
+    fn get(&self, key: &Slice) -> Result<Slice, Status> {
+        match self.table.get(key) {
+            Some(v) => {
+                let (seq,value_type, value) = v.value();
+                match value_type {
+                    ValueType::KTypeDeletion => Err(Status::not_found("not found", None)),
+                    _ => {
+                        let data = value.data();
+                        let mut key_len = 0;
+                        let key_ptr = get_varint32ptr(data, &mut key_len).unwrap();
+                        let v = get_length_prefixed_slice(&key_ptr[..key_len as usize]);
+                        Ok(v)
+                    } ,
+                }
+            }
+            None => Err(Status::not_found("not found", None)),
+        }
+        
     }
 }
